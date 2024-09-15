95791410afe97f08f5ab4cc743a08224a1832207
AleksandR <Smoker007@ya.ru>
2024-09-15 12:40:15 +0300

    HW_1

---
+++ HW_1/FamalyMembers.cs
@@ -0,0 +1,31 @@
+namespace hw1
+{
+
+    public enum Gender { Mуж, Жен }
+
+    public class FamalyMembers
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Surname { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public FamalyMembers Mother { get; set; }
+        public FamalyMembers Father { get; set; }
+
+        public FamalyMembers[] GetGrandParents()
+        {
+            return new FamalyMembers[] { Mother?.Mother, Mother?.Father, Father?.Mother, Father?.Father };
+        }
+
+        public FamalyMembers[] GetParents()
+        {
+            return new FamalyMembers[] { Mother, Father };
+        }
+
+        public string MemberInfo()
+        {
+            return $"=> {LastName} {Name} {Surname}\t (Пол {Gender}|Возраст {Age})";
+        }
+    }
+}
---
+++ HW_1/Program.cs
@@ -0,0 +1,96 @@
+﻿
+namespace hw1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FamalyMembers GrandFather1 = new()
+            {
+                Name = "Иван",
+                Surname = "Сергеевич",
+                LastName = "Иванов",
+                Gender = Gender.Mуж,
+                Age = 60
+            };
+
+            FamalyMembers GrandFather2 = new()
+            {
+                Name = "Аркадий",
+                Surname = "Николаевич",
+                LastName = "Петров",
+                Gender = Gender.Mуж,
+                Age = 64
+            };
+
+            FamalyMembers GrandMother1 = new()
+            {
+                Name = "Анна",
+                Surname = "Викторовна",
+                LastName = "Иванова",
+                Gender = Gender.Жен,
+                Age = 58
+            };
+
+            FamalyMembers GrandMother2 = new()
+            {
+                Name = "Лидия",
+                Surname = "Александровна",
+                LastName = "Петрова",
+                Gender = Gender.Mуж,
+                Age = 65
+            };
+
+            FamalyMembers Mother = new()
+            {
+                Name = "Лидия",
+                Surname = "Ивановна",
+                LastName = "Петрова",
+                Gender = Gender.Жен,
+                Age = 36,
+                Mother = GrandMother1,
+                Father = GrandFather1
+            };
+
+            FamalyMembers Father = new()
+            {
+                Name = "Петор",
+                Surname = "Аркадиевич",
+                LastName = "Петров",
+                Gender = Gender.Mуж,
+                Age = 73,
+                Mother = GrandMother2,
+                Father = GrandFather2
+            };
+
+            FamalyMembers Son = new()
+            {
+                Name = "Филипп",
+                Surname = "Петрович",
+                LastName = "Петров",
+                Gender = Gender.Mуж,
+                Age = 15,
+                Mother = Mother,
+                Father = Father
+            };
+
+            var GrandParents = Son.GetGrandParents();
+            var Parents = Son.GetParents();
+
+            Console.Write("Всем привет! давайте знакомиться, я\n" + Son.MemberInfo() + "\n");
+
+            Console.WriteLine("\nМоими родителями являются\n");
+
+            foreach (var item in Parents)
+            {
+                Console.WriteLine(item?.MemberInfo());
+            }
+
+            Console.WriteLine("\nУ меня есть Бабушки и Дедушки\n");
+            foreach (var item in GrandParents)
+            {
+                Console.WriteLine(item?.MemberInfo());
+            }
+        }
+    }
+}
