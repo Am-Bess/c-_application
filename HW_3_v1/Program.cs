6796e7ff08d7e5667e1fc4468afc28ae36426ac9
AleksandR <79250403617@ya.ru>
2024-09-23 22:51:30 +0300

    HW_3

---
+++ HW_3_v1/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[,] labirynth = new int[,]
+        {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {2, 0, 0, 0, 1, 0, 2 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 2, 1, 1, 1 }
+        };
+
+        Console.WriteLine($"Количество выходов = {HasExit(1, 1, labirynth)}");
+
+        int HasExit(int startX, int startY, int[,] l)
+        {
+            Stack<Tuple<int, int>> path = new Stack<Tuple<int, int>>();
+
+            if (l[startX, startY] == 0) path.Push(new(startX, startY));
+            int exitCount = 0;
+
+            while (path.Count > 0)
+            {
+                var current = path.Pop();
+
+                if (l[current.Item1, current.Item2] == 2)
+                    exitCount++;
+
+                l[current.Item1, current.Item2] = 1;
+
+                if (current.Item1 + 1 < l.GetLength(0)
+                && l[current.Item1 + 1, current.Item2] != 1)
+                    path.Push(new(current.Item1 + 1, current.Item2));
+
+                if (current.Item2 + 1 < l.GetLength(1) &&
+                l[current.Item1, current.Item2 + 1] != 1)
+                    path.Push(new(current.Item1, current.Item2 + 1));
+
+                if (current.Item1 > 0 && l[current.Item1 - 1, current.Item2] != 1)
+                    path.Push(new(current.Item1 - 1, current.Item2));
+
+                if (current.Item2 > 0 && l[current.Item1, current.Item2 - 1] != 1)
+                    path.Push(new(current.Item1, current.Item2 - 1));
+            }
+            return exitCount;
+        }
+    }
+}
---
+++ HW_3_v2/Program.cs
@@ -0,0 +1,59 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[,] labirynth = new int[,]
+        {
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 0 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 }
+        };
+
+        Console.WriteLine($"Количество выходов = {HasExit(1, 1)}");
+
+        int HasExit(int startX, int startY)
+        {
+            Stack<Tuple<int, int>> path = new Stack<Tuple<int, int>>();
+
+            if (labirynth[startX, startY] == 0) path.Push(new(startX, startY));
+            int exitCount = 0;
+
+            while (path.Count > 0)
+            {
+                var current = path.Pop();
+
+                if (isExit(current.Item1, current.Item2))
+                    exitCount++;
+
+                labirynth[current.Item1, current.Item2] = 1;
+
+                if (current.Item1 + 1 < labirynth.GetLength(0)
+                && labirynth[current.Item1 + 1, current.Item2] != 1)
+                    path.Push(new(current.Item1 + 1, current.Item2));
+
+                if (current.Item2 + 1 < labirynth.GetLength(1) &&
+                labirynth[current.Item1, current.Item2 + 1] != 1)
+                    path.Push(new(current.Item1, current.Item2 + 1));
+
+                if (current.Item1 > 0 && labirynth[current.Item1 - 1, current.Item2] != 1)
+                    path.Push(new(current.Item1 - 1, current.Item2));
+
+                if (current.Item2 > 0 && labirynth[current.Item1, current.Item2 - 1] != 1)
+                    path.Push(new(current.Item1, current.Item2 - 1));
+            }
+            return exitCount;
+        }
+
+        bool isExit(int x, int y)
+        {
+            if (x == 0 | y == 0) return true;
+            if (x + 2 > labirynth.GetLength(0) | y + 2 > labirynth.GetLength(1)) return true;
+            else return false;
+        }
+    }
+}
